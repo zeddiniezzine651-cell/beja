9b8e1cae5da0e0346dcfcb2ef364ce2b88aaaec6
zeddiniezzine651-cell <zeddiniezzine651@gmail.com>
2026-04-13 05:41:04 +0600

    Add files via upload

---
+++ map/styles/rue_2_style.js
@@ -0,0 +1,31 @@
+var size = 0;
+var placement = 'point';
+
+var style_rue_2 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("");
+    var labelFont = "10.4px \'Open Sans\', sans-serif";
+    var labelFill = "#f7130c";
+    var bufferColor = "#fafafa";
+    var bufferWidth = 3.0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'line';
+    if (feature.get("rue") !== null) {
+        labelText = String(feature.get("rue"));
+    }
+    var style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(17,17,178,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 3.8}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
